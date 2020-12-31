6288a17d1aedf3bdf38939b69e143c2883a40801
zhanglun <zhanglun.me@gmail.com>
2020-12-31 16:46:51 +0800

    :egg: 增加登录接口

---
+++ src/main/java/io/github/mrzhangboss/web/demo/UserController.java
@@ -0,0 +1,15 @@
+package io.github.mrzhangboss.web.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/api")
+public class UserController {
+    @RequestMapping(value = "/session", method = RequestMethod.POST)
+    public String login() {
+        return "ok";
+    }
+}
