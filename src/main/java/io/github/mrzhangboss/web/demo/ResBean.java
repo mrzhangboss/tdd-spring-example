61c687861359a68d56c26a8deb04a271fe0d869f
zhanglun <zhanglun.me@gmail.com>
2020-12-31 18:09:15 +0800

    :tea: 添加用户登录失败测试

---
+++ src/main/java/io/github/mrzhangboss/web/demo/ResBean.java
@@ -0,0 +1,47 @@
+package io.github.mrzhangboss.web.demo;
+
+public class ResBean<T> {
+    private int code;
+    private String msg;
+    private T data;
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public static ResBean<String> LoginError() {
+        ResBean<String> res = new ResBean<>();
+        res.setCode(500);
+        res.setMsg("用户名或者密码错误");
+        return res;
+    }
+
+    public static ResBean<String> LoginSuccess() {
+        ResBean<String> res = new ResBean<>();
+        res.setCode(200);
+        res.setMsg("ok");
+        return res;
+    }
+
+}
--- src/test/java/io/github/mrzhangboss/web/demo/UserTests.java
+++ src/test/java/io/github/mrzhangboss/web/demo/UserTests.java
@@ -1,5 +1,9 @@
 package io.github.mrzhangboss.web.demo;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +11,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.mockito.Mockito.anyString;
@@ -30,13 +35,49 @@ class UserTests {
     @Autowired
     private MockMvc mvc;
 
+    void testUser(String user, ResultMatcher content) throws Exception {
+        mvc.perform(MockMvcRequestBuilders.post("/api/session").accept(MediaType.APPLICATION_JSON).contentType(MediaType.APPLICATION_JSON).content(user))
+                .andExpect(status().isOk())
+                .andExpect(content);
+
+    }
+
+    String obj2json(Object obj) {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.configure(SerializationFeature.WRAP_ROOT_VALUE, false);
+        ObjectWriter ow = mapper.writer().withDefaultPrettyPrinter();
+        String json = null;
+        try {
+            json = ow.writeValueAsString(obj);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+            return "";
+        }
+        return json;
+    }
+
+
+    String getUser(String password) throws Exception {
+        UserBean user = new UserBean();
+        user.setUser("admin");
+        user.setPassword(password);
+        return obj2json(user);
+    }
+
+    String getInfo(int code) {
+        ResBean<String> bean = new ResBean<>();
+        bean.setCode(code);
+        return obj2json(bean);
+
+    }
 
     @Test
     void testLoginSession() throws Exception {
-		String user = "{\"user\": \"admin\", \"password\": \"1234567\"}";
-        mvc.perform(MockMvcRequestBuilders.post("/api/session").accept(MediaType.APPLICATION_JSON).contentType(MediaType.APPLICATION_JSON).content(user))
-                .andExpect(status().isOk())
-                .andExpect(content().string(equalTo("ok")));
+        String user = getUser("1234567");
+        String errorUser = getUser("12345679");
+        testUser(user,  content().string(equalTo(obj2json(ResBean.LoginSuccess()))));
+        testUser(errorUser, content().string(equalTo(obj2json(ResBean.LoginError()))));
+
 
     }
 
