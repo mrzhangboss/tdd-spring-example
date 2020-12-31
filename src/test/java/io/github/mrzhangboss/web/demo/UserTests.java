90a96aad8c338f214e16ad4205f55ed5b1cefa56
zhanglun <zhanglun.me@gmail.com>
2020-12-31 16:43:11 +0800

    :tea: 用户登录测试

---
+++ src/test/java/io/github/mrzhangboss/web/demo/UserTests.java
@@ -0,0 +1,43 @@
+package io.github.mrzhangboss.web.demo;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.mockito.Mockito.anyString;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.hamcrest.Matchers.equalTo;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class UserTests {
+    @Autowired
+    private MockMvc mvc;
+
+
+    @Test
+    void testLoginSession() throws Exception {
+		String user = "{\"user\": \"admin\", \"password\": \"1234567\"}";
+        mvc.perform(MockMvcRequestBuilders.post("/api/session").accept(MediaType.APPLICATION_JSON).contentType(MediaType.APPLICATION_JSON).content(user))
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("ok")));
+
+    }
+
+}
